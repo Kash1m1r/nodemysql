a0d34d9f190312ac38f1dcb0d26d5e2d6873e3ee
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2022-09-27 10:27:07 -0300

    CrudUpdate

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,7 +1,7 @@
 const database = require('../models')
 
 class PessoaController {
-    static async puxarTodasPessoas(req, res){
+    static async puxarTodasPessoas(req, res) {
         try {
             const allPessoas = await database.Pessoas.findAll();
             return res.status(200).json(allPessoas)
@@ -10,6 +10,44 @@ class PessoaController {
         }
       
     }
+
+    static async puxarUmaPessoa(req, res) {
+        const { id }  = req.params
+        try {          
+            const umaPessoa = await database.Pessoas.findOne( {where: {id: Number(id)}} )
+            return res.status(200).json(umaPessoa);
+        }catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async criarPessoa(req, res) {
+        const novaPessoa = req.body
+        try{
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada);
+
+        }catch{
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async atualizarPessoa(req, res) {
+        const { id } = req.params;
+        const novaInfo = req.body;
+
+        try{
+            await database.Pessoas.update(novaInfo, {where: {id: Number(id)}});
+            const pessoaAtualizada = await database.Pessoas.findOne({where: {id: Number(id)}});
+            return res.status(200).json(pessoaAtualizada);
+        }catch (error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    
+
+    
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,8 +1,11 @@
-const { Router } = require('express')
-const PessoaController = require('../controllers/PessoaController')
+const { Router } = require('express');
+const PessoaController = require('../controllers/PessoaController');
 
-const router = Router()
+const router = Router();
 
-router.get('/pessoas', PessoaController.puxarTodasPessoas)
+router.get('/pessoas', PessoaController.puxarTodasPessoas);
+router.get('/pessoas/:id', PessoaController.puxarUmaPessoa);
+router.post('/pessoas', PessoaController.criarPessoa);
+router.put('/pessoas', PessoaController.atualizarPessoa);
 
 module.exports = router;
